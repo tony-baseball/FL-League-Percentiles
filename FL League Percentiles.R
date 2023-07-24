993c2d75af3c72db2e7e1eb8e95ffa6d447fffac
tony-baseball <tdmedina21@gmail.com>
2023-07-24 18:28:31 -0500

    Update FL League Percentiles.R

--- FL League Percentiles.R
+++ FL League Percentiles.R
@@ -22,7 +22,7 @@ h_yak <- yak %>%
             `Max Exit Velo` = round(max(ExitSpeed, na.rm = TRUE),1),
             `Hard Hit%` = round((sum(hardhit, na.rm = T) / sum(PitchCall=='InPlay'& !is.na(ExitSpeed), na.rm = T) )*100,2),
             `Whiff %` = round((sum(whiff, na.rm = T) / sum(swing,na.rm = T))*100,1),
-            `Chase %` = round((sum(swing, na.rm = T) / sum(in_zone==0,na.rm = T))*100,1)
+            `Chase %` = round((sum(swing[in_zone==0], na.rm = T) / sum(in_zone==0,na.rm = T))*100,1)
             
   )
 # join normal stats and yakkertech stats
@@ -38,7 +38,7 @@ h_full_pivot <- h_full %>%
 
 # create percentiles DF
 h_percentiles <- h_full %>%
-  mutate(across(all_of(stats_columns), ~rank(., ties.method = "min") / n() * 100),
+  mutate(across(all_of(stats_columns), ~ (rank(., na.last = "keep") / sum(!is.na(.))) * 100),
          across(all_of(stats_columns), ~ round(.)))  %>%
   pivot_longer(cols = starts_with(c(stats_columns)),
                names_to = "Stat",
@@ -49,13 +49,17 @@ h_percentiles <- h_full %>%
                                         "BB%", "K%", 'Whiff %', "Chase %")),
          Percentile = ifelse(Stat %in% c("K%", "Whiff %", "Chase %"), 100 - Percentile, Percentile))%>%
   group_by(Stat) %>%
+  mutate(Rank = ifelse(Stat %in% c("BB%", "ERA", 'FIP', 'vwOBA', 'vSLG',"Avg. Exit Velo",  "Hard Hit%"), 
+                       dense_rank(Value), dense_rank(desc(Value))),
+         UOM = ifelse(Stat %in% c("BB%", "K%", "Whiff %", "Hard Hit%", 'Chase%'), "%",
+                      ifelse(Stat %in% c('Max Exit Velo', 'Avg. Exit Velo'), ' mph',
+                             ifelse(Stat %in% c('Fastball Spin'), " rpm",
+                                    ''))) ) %>%
   mutate(Rank = ifelse(Stat %in% c("K%", "Whiff %", "Chase %"), 
-                       dense_rank(Value), dense_rank(desc(Value)) 
-  ) 
-  ) %>%
+                       dense_rank(Value), dense_rank(desc(Value))) ) %>%
   ungroup()
 
-hitter <- h_percentiles %>% filter(NAME == "Chase Dawson") 
+hitter <- h_percentiles %>% filter(NAME == "Blake Berry") 
 
 ggplotly(
   ggplot(hitter, aes(text = paste0(Stat,": ", Value,
@@ -72,6 +76,8 @@ ggplotly(
     ggtitle("Frontier League Hitter Percentile Rankings")+
     geom_text(aes(x = Percentile, y = Stat, label = Percentile), size = 4, fontface = 'bold',  
               color = ifelse(hitter$Percentile > 25 & hitter$Percentile < 75, "black", "white")) +
+    geom_text(aes(x = 50, y = 1.4, label = paste0(Value, UOM)), size = 3.5, fontface = 'bold',  
+              color = 'black') +
     facet_wrap(~ interaction(y = Stat), scales='free_y', ncol = 4) +
     theme(panel.grid = element_blank(),
           axis.title = element_blank(),
@@ -83,7 +89,21 @@ ggplotly(
           strip.text = element_text(size = 12, face = 'bold')
     )  
 ) %>%
-  layout(title = 'Frontier League Hitter Percentile Rankings\n', plot_bgcolor = "white")
+  layout(title = 'Frontier League Hitter Percentile Rankings\n', plot_bgcolor = "white") %>%  
+  layout( xaxis = list(title = ""), yaxis = list(title = ""),
+          images = list(  
+            list(  
+              source =  "https://i.imgur.com/tQZCji9.png",  
+              xref = "paper",  
+              yref = "paper",  
+              x = 0.5,  
+              y = 0.05,  
+              sizex = 0.25,  
+              sizey = 0.25,  
+              xanchor="center",  
+              yanchor="center" 
+            )  
+          ) ) 
 
 
 # ---------------- FRONTIER PITCHERS  ------------
@@ -101,11 +121,11 @@ p <- read.csv("C:/Users/tdmed/OneDrive/_Shiny/_Coop2/frontier_all_pitchers23.csv
 p_yak <- yak %>%
   group_by(Pitcher) %>%
   summarise(`Fastball Velo` = round(mean(RelSpeed[TaggedPitchType %in% c('Fastball', 'Sinker')], na.rm = TRUE),1),
-            `Fastball Spin` = round(mean(SpinRate[TaggedPitchType %in% c('Fastball', 'Sinker')], na.rm = TRUE)),
-            `Hard Hit%` = round((sum(hardhit, na.rm = T) / sum(PitchCall=='InPlay'& !is.na(ExitSpeed), na.rm = T) )*100),
-            `Whiff %` = round((sum(whiff, na.rm = T) / sum(swing,na.rm = T))*100),
-            `Avg. Exit Velo` = round(mean(ExitSpeed[PitchCall=='InPlay'], na.rm = TRUE)),
-            `Chase %` = round((sum(swing, na.rm = T) / sum(in_zone==0,na.rm = T))*100)
+            `Fastball Spin` = round(mean(SpinRate[TaggedPitchType %in% c('Fastball', 'Sinker')], na.rm = TRUE),1),
+            `Hard Hit%` = round((sum(hardhit, na.rm = T) / sum(PitchCall=='InPlay'& !is.na(ExitSpeed), na.rm = T) )*100,1),
+            `Whiff %` = round((sum(whiff, na.rm = T) / sum(swing,na.rm = T))*100,1),
+            `Avg. Exit Velo` = round(mean(ExitSpeed[PitchCall=='InPlay'], na.rm = TRUE),1),
+            `Chase %` = round((sum(swing[in_zone==0], na.rm = T) / sum(in_zone==0,na.rm = T))*100,1)
             
   )
 
@@ -120,7 +140,7 @@ p_full_pivot <- p_full %>%
                values_to = "Value")
 
 p_percentiles <- p_full %>%
-  mutate(across(all_of(p_stats_columns), ~rank(., ties.method = "min") / n() * 100),
+  mutate(across(all_of(p_stats_columns), ~ (rank(., na.last = "keep") / sum(!is.na(.))) * 100),
          across(all_of(p_stats_columns), ~ round(.)))  %>%
   pivot_longer(cols = starts_with(c(p_stats_columns)),
                names_to = "Stat",
@@ -131,12 +151,15 @@ p_percentiles <- p_full %>%
          Percentile = ifelse(Stat %in% c("BB%", "ERA", 'FIP', 'vwOBA', 'vSLG',"Avg. Exit Velo",  "Hard Hit%"), 100 - Percentile, Percentile)) %>%
   group_by(Stat) %>%
   mutate(Rank = ifelse(Stat %in% c("BB%", "ERA", 'FIP', 'vwOBA', 'vSLG',"Avg. Exit Velo",  "Hard Hit%"), 
-                       dense_rank(Value), dense_rank(desc(Value)) 
-  ) 
+                       dense_rank(Value), dense_rank(desc(Value))),
+         UOM = ifelse(Stat %in% c("BB%", "K%", "Whiff %", "Hard Hit%", 'Chase%'), "%",
+                      ifelse(Stat %in% c('Fastball Velo', 'Avg. Exit Velo'), ' mph',
+                             ifelse(Stat %in% c('Fastball Spin'), " rpm",
+                                    '')))
   ) %>%
   ungroup()
 
-pitcher <- p_percentiles %>% filter(NAME == "Kristian Scott") 
+pitcher <- p_percentiles %>% filter(NAME == "Merfy Andrew") 
 
 ggplotly(
   ggplot(pitcher, aes(text = paste0(Stat,": ", Value,
@@ -153,6 +176,8 @@ ggplotly(
     ggtitle("Frontier League Pitcher Percentile Rankings")+
     geom_text(aes(x = Percentile, y = Stat, label = Percentile), size = 4, fontface = 'bold',  
               color = ifelse(pitcher$Percentile > 25 & pitcher$Percentile < 75, "black", "white")) +
+    geom_text(aes(x = 50, y = 1.4, label = paste0(Value,UOM) ), size = 3.5, fontface = 'bold',  
+              color = 'black')+
     facet_wrap(~ interaction(y = Stat), scales='free_y', ncol = 4) +
     theme(plot.title = element_text(hjust = .5),
           panel.grid = element_blank(),
@@ -165,7 +190,21 @@ ggplotly(
           strip.text = element_text(size = 12, face = 'bold')
     )  
 ) %>%
-  layout(title = 'Frontier League Pitcher Percentile Rankings\n', plot_bgcolor = "white")
+  layout(title = 'Frontier League Pitcher Percentile Rankings\n', plot_bgcolor = "white")%>%  
+  layout( xaxis = list(title = ""), yaxis = list(title = ""),
+          images = list(  
+            list(  
+              source =  "https://i.imgur.com/tQZCji9.png",  
+              xref = "paper",  
+              yref = "paper",  
+              x = 0.5,  
+              y = 0.05,  
+              sizex = 0.25,  
+              sizey = 0.25,  
+              xanchor="center",  
+              yanchor="center" 
+            )  
+          ) ) 
 
 # ------- RADAR ----------
 # h_full_ranks <- h_full %>%
